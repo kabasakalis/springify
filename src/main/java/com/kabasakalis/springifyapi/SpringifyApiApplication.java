38d00f2e3ed510029c5c0c8e67ad954877ddd3fb
Spiros Kabasakalis <kabasakalis@gmail.com>
2017-12-02 14:57:54 +0200

    First Commit

---
+++ src/main/java/com/kabasakalis/springifyapi/SpringifyApiApplication.java
@@ -0,0 +1,12 @@
+package com.kabasakalis.springifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringifyApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kabasakalis/springifyapi/SpringifyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kabasakalis.springifyapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringifyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
