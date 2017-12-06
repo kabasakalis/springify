e05479c62d9a548eebf1908f6857e9fcc42d63a0
Spiros Kabasakalis <kabasakalis@gmail.com>
2017-12-06 02:28:31 +0200

    Added migration task

--- src/main/db/migrations/002_populate_artists.sql
+++ src/main/db/migrations/002_populate_artists.sql
@@ -4,3 +4,4 @@ insert into artists (id, name) values (1, 'King Diamond');
 insert into artists (id, name) values (2, 'Iron Maiden');
 insert into artists (id, name) values (3, 'Judas Priest');
 insert into artists (id, name) values (4, 'Savatage');
+insert into artists (id, name) values (5, 'Post Malone');
--- src/main/java/com/kabasakalis/springifyapi/SpringifyApiApplication.java
+++ src/main/java/com/kabasakalis/springifyapi/SpringifyApiApplication.java
@@ -2,11 +2,25 @@ package com.kabasakalis.springifyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Properties;
+ import java.net.URL;
+import java.net.URLClassLoader;
 
 @SpringBootApplication
 public class SpringifyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringifyApiApplication.class, args);
+
+   Properties props = System.getProperties();
+   props.list(System.out);
+
+ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
 	}
 }
