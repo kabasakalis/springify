0aee0177a193bdd938627bf72ad2e95a085044f7
Spiros Kabasakalis <kabasakalis@gmail.com>
2017-12-16 02:28:23 +0200

    Base Path refactoring

--- src/main/java/com/kabasakalis/springifyapi/controllers/CoreController.java
+++ src/main/java/com/kabasakalis/springifyapi/controllers/CoreController.java
@@ -3,6 +3,10 @@ package com.kabasakalis.springifyapi.controllers;
 import org.springframework.hateoas.Link;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController("/api")
 public class CoreController {
     protected Link createHateoasLink(long id){
         Link link = linkTo(getClass()).slash(id).withSelfRel();
