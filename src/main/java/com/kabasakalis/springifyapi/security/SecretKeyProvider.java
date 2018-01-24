25ab6db8b6c9f2ac46e164ef4ecd034eaa85cd98
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-01-24 11:43:22 +0200

    Configure heroku

--- src/main/java/com/kabasakalis/springifyapi/security/SecretKeyProvider.java
+++ src/main/java/com/kabasakalis/springifyapi/security/SecretKeyProvider.java
@@ -3,13 +3,30 @@ package com.kabasakalis.springifyapi.security;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.util.Collections;
 
 @Component
 public class SecretKeyProvider {
     public byte[] getKey() throws URISyntaxException, IOException {
-        return Files.readAllBytes(Paths.get(this.getClass().getResource("/jwt.key").toURI()));
+
+        String jwtkey = "/jwt.key";
+        URI uri = getClass().getResource(jwtkey).toURI();
+        Path path;
+        FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
+        try {
+            if (uri.getScheme().equals("jar")) {
+                path = fileSystem.getPath("/BOOT-INF/classes" + jwtkey);
+            } else {
+                // Not running in a jar, so just use a regular filesystem path
+                path = Paths.get(uri);
+            }
+
+            return Files.readAllBytes(path);
+        } finally {
+            fileSystem.close();
+        }
     }
 }
